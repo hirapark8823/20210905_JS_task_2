e28b5671829ec91342e67dd0fe9fd0b213e53da2
hirapark8823 <90318741+hirapark8823@users.noreply.github.com>
2021-09-17 17:38:42 +0900

    初JAVA

---
+++ 20210905task2_xi(notfinished)/.vscode/xi_board.js
@@ -0,0 +1,13 @@
+// $(document).ready(function () {
+//     alert("jQuery Test Alert.");
+// })
+
+// 東京編
+$('#trip_btn').on('click', function () {
+    $('#xi_item1').text('東京から船で青ヶ島！5時間')
+    $('#xi_item2').text('東京から夜行列車で出雲！14時間');
+    $('#xi_item3').text('東京から飛行機で福岡GOAL！３時間');
+    $('#xi_item4').text('東京から飛行機で鹿児島！4時間');
+    $('#xi_item5').text('東京から深夜バスで青森！12時間');
+    $('#xi_item6').text('東京から新幹線で大阪！2時間');
+});
